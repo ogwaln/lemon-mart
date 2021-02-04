320bdb4ecc3eb530ef804bfdb38eb2e8ff56e343
ogwaln <ogwaln@hotmail.com>
2021-02-03 20:42:12 -0600

    server issuse #16 add env config to fix CI

---
+++ src/environments/environment.lemon-mart-server.ts
@@ -0,0 +1,17 @@
+import { AuthMode } from 'src/app/auth/auth.enum'
+
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost:3000',
+  authMode: AuthMode.Firebase,
+  firebase: {
+    apiKey: 'AIzaSyCyv_ZFRrFHa9uxmce3WC1sd5ejS7TzNlU',
+    authDomain: 'lemon-mart-6aa50.web.app',
+    databaseURL: 'https://lemon-mart-6aa50.webio.app/',
+    projectId: 'lemon-mart-6aa50',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '1:717215363293:web:2e39cc954905021317eb34',
+    measurementId: '',
+  },
+}
